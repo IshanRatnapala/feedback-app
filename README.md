b42770f154257b87ee6d501451a089bc126f8114
Dragfreeze <Dragfreeze@gmail.com>
2016-09-18 13:16:32 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# make-me-better
+
+“We all need people who will give us feedback. That’s how we improve.”
+– Bill Gates
+
+A web application that facilitates better communication within a team. Lets you give feedback to your colleagues and let them comment on you.
+
+
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name codepoolApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the codepoolApp
+ */
+angular.module('codepoolApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
